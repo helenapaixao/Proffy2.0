3acbee7ab250d4138a52da25b3d574f3788e328d
Helena Paixão <hp.helenapaixao@gmail.com>
2020-08-27 21:45:00 -0400

    add authprovider

---
+++ src/hooks/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+import {AuthProvider} from './auth';
+
+
+const AppProvider: React.FC = ({children}) => (
+    <AuthProvider>
+       
+            {children}
+
+    </AuthProvider>
+);
+
+export default AppProvider;
