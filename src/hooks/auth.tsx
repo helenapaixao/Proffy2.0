bba2fc91c28324ec39acad0ab8d25fce1eaae027
Helena Paixão <hp.helenapaixao@gmail.com>
2020-08-27 21:40:17 -0400

    config hookks

---
+++ src/hooks/auth.tsx
@@ -0,0 +1,87 @@
+import React, { createContext, useCallback, useState, useContext } from 'react';
+
+
+
+
+interface User {
+    id: number;
+    name: string;
+    email: string;
+    password: string;
+    cpf: number;
+    avatar_url: string;
+    endereco: string;
+    numero: number;
+    rua: string;
+    cidade: string;
+}
+
+
+interface AuthState {
+    id: string;
+    name: string;
+}
+
+interface signInCredentials {
+    email: string;
+    password: string;
+}
+
+interface AuthContextData {
+    id: string;
+    signIn(credentials: signInCredentials): Promise<void>;
+    signOut(): void;
+}
+
+const AuthContext = createContext<AuthContextData>({} as AuthContextData);
+
+const AuthProvider: React.FC = ({ children }) => {
+    const [data, setData] = useState<AuthState>(() => {
+        const id = localStorage.getItem('@GoBarber:token');
+        const name = localStorage.getItem('@GoBarber:user');
+
+        if (id && name) {
+            return { id, name };
+        }
+
+        return {} as AuthState;
+    });
+
+    const signIn = useCallback(async ({ email, password }) => {
+       /*  const response = await api.post('sessions', {
+            email,
+            password,
+        }); */
+
+    /*     const { id, name } = response.data; */
+
+       /*  localStorage.setItem('@Proffy:token', id);
+        localStorage.setItem('@Proffy:user', name);
+
+        setData({ id, name }); */
+    }, []);
+
+    const signOut = useCallback(() => {
+        localStorage.removeItem('@Proffy:token');
+        localStorage.removeItem('@Proffy:user');
+
+        setData({} as AuthState);
+    }, []);
+
+    return (
+        <AuthContext.Provider value={{ id: data.id, signIn, signOut }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+function useAuth(): AuthContextData {
+    const context = useContext(AuthContext);
+
+    if (!context) {
+        throw new Error('useAuth must be used within a AuthProvider');
+    }
+    return context;
+}
+
+export { AuthProvider, useAuth };
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React,{useRef,useCallback} from "react";
 import { Container, Content, Background, AnimationContainer } from "./styles";
 
 import { FiLogIn, FiMail, FiLock } from "react-icons/fi";
 import { FormHandles } from "@unform/core";
 import { Form } from "@unform/web";
 import * as Yup from "yup";
+import getValidationErrors from '../../utils/getValidationErrors';
 
 import { Link } from "react-router-dom";
 import { useHistory } from "react-router";
@@ -12,7 +13,52 @@ import { useHistory } from "react-router";
 import Input from "../../components/Input";
 import Button from "../../components/Button";
 
+import Logo from '../../assets/Logo.svg';
+import { useAuth } from '../../hooks/auth';
+
+interface LoginFormData {
+    email:string;
+    password: string;
+}
+
+
 const Login: React.FC = () => {
+    const formRef = useRef<FormHandles>(null);
+    const { signIn } = useAuth();
+    const history = useHistory();
+
+
+    const handleSubmit = useCallback(
+        async (data:LoginFormData) => {
+            try {
+                formRef.current?.setErrors({});
+                const shema = Yup.object().shape({
+                    email: Yup.string().required('Email obrigatório').email('Digite um e-mail válido'),
+                    password: Yup.string().required('Senha Obrigatória'),
+                })
+
+                await shema.validate(data, {
+                    abortEarly:false,
+                })
+
+                await signIn({
+                    email:data.email,
+                    password:data.password,
+                });
+                history.push('/dashboard');
+
+
+            } catch (err) {
+                if(err instanceof Yup.ValidationError) {
+                    const errors = getValidationErrors(err);
+                    formRef.current?.setErrors(errors);
+                    return;
+                }
+            }
+        },
+        [history, signIn],
+    )
+
   return (
     <>
       <Container>
