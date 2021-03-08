034e71847445e16c76efc67c48dec5be72285d3c
Helena Paixão <hp.helenapaixao@gmail.com>
2021-03-08 20:08:52 -0300

    fix:config login page

---
+++ src/routes/Route.tsx
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Login from '../pages/Login';
+import Register from '../pages/Register';
+
+const Routes: React.FC = () => (
+  <Switch>
+    <Route exact path="/" component={Login} />
+    <Route path="/Register" component={Register} />
+  </Switch>
+);
+
+export default Routes;
